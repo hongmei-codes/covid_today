783d8b35b3dedf17349a4a134e36bb7e9f728f53
hongmei <duhongmei@protonmail.com>
2020-07-25 01:10:42 +0800

    File restructure and update README.md

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
 <div align="center">
     <h1>😷 Covid Today</h1>
-    <p>A simple dashboard that visualises 🦠 covid-19 cases today.</p>
+    <p>A simple dashboard that visualises 🦠 covid-19 data. Go to the demo page to interact with the data or read more about its features below.</p>
+
+[![Website](https://img.shields.io/website?down_color=red&down_message=offline&label=Demo&style=for-the-badge&up_color=b&up_message=online&url=https%3A%2F%2Fh0ngm3i.pythonanywhere.com)](http://h0ngm3i.pythonanywhere.com/)
+
 </div>
 <br/>
 
+# Features
 
+## Interactive world map bubble chart
+![map](https://github.com/hongmei-codes/covid_today/blob/master/demo/map.gif)
+
+The dashboard features some basic data about the covid-19 cases around the world. The star 🌟 of the dashboard is the interative geo-bubble chart. You can zoom in, zoom out, navigate to which ever country or city you are interested. Clicking the bubbles will trigger a popup which shows the number of covid-19 cases on that day for that particular location. From a zoomed out view, the density of the bubbles also helps you to visualise the hard hit areas.
+
+Interact with the map at this [demo](http://h0ngm3i.pythonanywhere.com/).
+
+## Responsive
+![responsive](https://github.com/hongmei-codes/covid_today/blob/master/demo/responsive.gif)
+
+The dashboard is designed to be responsive so that you can view it on any device you like. As the page width changes, the dashboard will readjust and restructure.
+
+## Updated data
+Everytime you visit the site, attemps will be made to fetch new data from [John Hopkins University's](https://github.com/CSSEGISandData/COVID-19) latest daily report. This means you will always get the latest update!
+
+
+# How was this built? 🤔
+## Data Manipulation
+The dataset used for the project is from [John Hopkins University](https://github.com/CSSEGISandData/COVID-19). Required data for the dashboard is cleaned and extracted using the powerful [pandas](https://github.com/pandas-dev/pandas) library. The latitude and longitude from JHU's dataset are extract and used to plot bubbles on the map.
+
+## Map
+The map is created using [folium](https://github.com/python-visualization/folium). As seen above, the map uses bubble to represent number of confirmed covid-19 cases. Larger confirmed case numbers, means larger circles. The best thing about using folium is that it takes in pandas dataframe objects directly, so there is no need to convert pandas dataframe into other data structures.
+
+To use follium, first install it will pip or anaconda. 
+```console
+$ pip install folium
+```
+or
+```console
+$ conda install folium -c conda-forge
+```
+
+Now you can create a folium map object with the code below.
+```python
+bubble_map = folium.Map(titles='Stamen toner')
+```
+
+Next, add bubbles to any coordinates using the   `.Circle()`  method.
+```python
+folium.Circle(location=[-36, 45], radius=1000, color='red').app_to(bubble_map)
+```
+
+## Web application
+The data visualisation web app is created using [flask](https://github.com/pallets/flask). This lighweight framework is easy to work with and can speed up the development process.
+
+## Responsive
+CSS styling is used to make the site responsive. To restructure the different components as width changes, you can use media queries.
+```css
+@media (max-width: 1000px) {
+  .dash {
+    flex-direction: column;
+  }
+```
+The styling above rearranges flexboxes as columns when the screen size hits below 1000px. 
+
+## Technology used
+- [X] python
+- [X] pandas
+- [X] folium
+- [X] flask
+- [X] html
+- [X] css
--- app.py
+++ app.py
@@ -1,7 +1,105 @@
 from flask import Flask, render_template
-from data import get_raw_data, world_total, top_ten, create_map
+import pandas as pd
+import folium  # to generate map
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+
+def get_raw_data():
+    """
+    This function gets daily covid-19 data from JHU
+    :return: utc_date, raw_data and data_url
+    """
+    # get utc date
+    utc_datetime = datetime.utcnow()
+    utc_date = utc_datetime.strftime('%m-%d-%Y')
+
+    # construct url
+    data_url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{utc_date}.csv'
+
+    # try except url
+    try:
+        # run this code
+        raw_data = pd.read_csv(data_url)
+    except Exception as e:
+        # run the following if there is an error
+        utc_date = (utc_datetime - timedelta(days=1)).strftime('%m-%d-%Y')
+        data_url = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{utc_date}.csv'
+        raw_data = pd.read_csv(data_url)
+    else:
+        # no error? run this
+        raw_data = pd.read_csv(data_url)
+
+    # return utc_date, dataframe and url
+    return utc_date, raw_data, data_url
+
+
+def world_total(original_df):
+    """
+    This function sums up cases of confirmed, active, deaths, recovered
+    all over the world.
+
+    :param original_df: raw, unprocessed pandas dataframe
+    :return: confirmed, active, deaths, recovered. all int
+    """
+    all_sum = original_df.sum()
+
+    confirmed = int(all_sum['Confirmed'])
+    active = int(all_sum['Active'])
+    deaths = int(all_sum['Deaths'])
+    recovered = int(all_sum['Recovered'])
+
+    return confirmed, active, deaths, recovered
+
+
+def top_ten(original_df):
+    """
+    Takes raw data and returns a list of top ten countries by confirmed cases
+    :param original_df: raw, unprocessed pandas dataframe
+    :return: pandas dataframe of top ten countries by confirmed cases
+    """
+    top_ten_table = original_df.groupby('Country_Region')\
+        .sum()[['Confirmed']]\
+        .sort_values(by=['Confirmed'], ascending=False)\
+        .nlargest(10, 'Confirmed').to_html()
+
+    tbody_start_index = top_ten_table.index('</thead>') + 8
+    html_table_body = top_ten_table[tbody_start_index:]
+
+    return html_table_body
+
+
+def add_bubble(bubble_map, data):
+    """
+    Adds bubbles to map object based on data passed in.
+    :param bubble_map: this is a folium Map object
+    :param data: this is a list of [location name, latitude, longitude, confirmed cases]
+    :returns: does not return anything
+    """
+    folium.Circle(location=[data[1], data[2]],
+                  radius=float(data[3]) * 1,
+                  popup=f'{data[0]}\nConfirmed cases: {data[3]}',
+                  color='#3186cc', fill=True, fill_color='#3186cc').add_to(bubble_map)
+
+
+def create_map(original_df):
+    """
+    This function creates a dataframe with required columns from original_df (raw data).
+    The new dataframe will be used to create a bubble map chart.
+    :param original_df: a pandas dataframe of raw data
+    :return: folium map object
+    """
+
+    # get required data
+    new_df = original_df[['Combined_Key', 'Lat', 'Long_', 'Confirmed']].dropna()
+
+    # create folium map object
+    bubble_map = folium.Map(tiles='Stamen toner')
+    new_df.apply(lambda x: add_bubble(bubble_map, x), axis=1)
+    html_bubble_map = bubble_map._repr_html_()
+
+    return html_bubble_map
 
 
 @app.route('/')
